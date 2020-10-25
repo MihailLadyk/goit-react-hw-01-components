cfee6c4759ebabb087cc458b3a3cfa1d5219f573
MihailLadyk <lp346426201@gmail.com>
2020-10-25 10:19:36 +0200

    Updates

---
+++ precache-manifest.e4bd02c5e80bf1e59f5cfabef2078c10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e41b42236f9bb3048d4d54ccea3ee1",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "a34195029bf5df3cff28",
+    "url": "/goit-react-hw-01-components/static/css/main.25d28fd4.chunk.css"
+  },
+  {
+    "revision": "8409a775af46fae5c5c3",
+    "url": "/goit-react-hw-01-components/static/js/2.4b027ab7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.4b027ab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a34195029bf5df3cff28",
+    "url": "/goit-react-hw-01-components/static/js/main.1833d934.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
